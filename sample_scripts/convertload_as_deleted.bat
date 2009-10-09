0c3a64a5f820be073c7c1dc7fd0a5166b8618e85
svakil@library.rochester.edu <svakil@library.rochester.edu@3eaa8e94-081b-11de-9c5c-7db7966a26d7>
2009-10-09 14:31:34 +0000

    git-svn-id: https://xcoaitoolkit.googlecode.com/svn/trunk@50 3eaa8e94-081b-11de-9c5c-7db7966a26d7

--- sample_scripts/convertload_as_deleted.bat
+++ sample_scripts/convertload_as_deleted.bat
@@ -7,5 +7,5 @@ rem parameters according to your needs. Read the
 rem OAIToolkit Manual for the details.
 rem ==============================================
 
-java -Xmx1024m -jar lib\OAIToolkit-0.6.1alpha.jar -load -convert -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema\MARC21slim_rochester.xsd -marc_encoding ISO_8859_1 -char_conversion MARC8 -split_size 10000 -storage_type Lucene -lucene_index lucene_index -translate_leader_bad_chars_to_zero -translate_nonleader_bad_chars_to_spaces -fileof_deleted_records
+java -Xmx1024m -jar lib\OAIToolkit-0.6.2alpha.jar -load -convert -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema\MARC21slim_rochester.xsd -marc_encoding ISO_8859_1 -char_conversion MARC8 -split_size 10000 -storage_type Lucene -lucene_index lucene_index -translate_leader_bad_chars_to_zero -translate_nonleader_bad_chars_to_spaces -fileof_deleted_records
 
