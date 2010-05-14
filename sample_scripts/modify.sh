ef61c47030d1a852e94cc8993ceb573d992fa9f6
sranganathan@library.rochester.edu <sranganathan@library.rochester.edu@3eaa8e94-081b-11de-9c5c-7db7966a26d7>
2010-05-14 21:48:40 +0000

    Updated scripts to point to 0.6.5 jar
    git-svn-id: https://xcoaitoolkit.googlecode.com/svn/trunk@81 3eaa8e94-081b-11de-9c5c-7db7966a26d7

--- sample_scripts/modify.sh
+++ sample_scripts/modify.sh
@@ -7,4 +7,4 @@ rem parameters according to your needs. Read the
 rem OAIToolkit Manual for the details.
 rem ==============================================
 
-java -Xmx1024m -jar lib/OAIToolkit-0.6.4alpha.jar -modify populate003.xsl -source dest_xml -destination modified_xml -destination xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -marc_encoding utf8
+java -Xmx1024m -jar lib/OAIToolkit-0.6.5alpha.jar -modify populate003.xsl -source dest_xml -destination modified_xml -destination xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -marc_encoding utf8
