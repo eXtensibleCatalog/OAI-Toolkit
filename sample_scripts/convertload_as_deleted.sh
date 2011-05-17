8121fe344089ffd241e3754347d2447943519727
cedelis@uillinois.edu <cedelis@uillinois.edu@3eaa8e94-081b-11de-9c5c-7db7966a26d7>
2011-05-17 20:41:28 +0000

    remove -storage_type from sample scripts
    git-svn-id: https://xcoaitoolkit.googlecode.com/svn/trunk@155 3eaa8e94-081b-11de-9c5c-7db7966a26d7

--- sample_scripts/convertload_as_deleted.sh
+++ sample_scripts/convertload_as_deleted.sh
@@ -7,4 +7,4 @@
 # OAIToolkit Manual for the details.
 # ==============================================
 
-java -Xmx1024m -jar lib/OAIToolkit-1.0.1.jar -load -convert -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -marc_encoding utf8 -split_size 10000 -storage_type Lucene -lucene_index lucene_index -translate_leader_bad_chars_to_zero -translate_nonleader_bad_chars_to_spaces -fileof_deleted_records
+java -Xmx1024m -jar lib/OAIToolkit-1.0.1.jar -load -convert -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -marc_encoding utf8 -split_size 10000 -lucene_index lucene_index -translate_leader_bad_chars_to_zero -translate_nonleader_bad_chars_to_spaces -fileof_deleted_records
