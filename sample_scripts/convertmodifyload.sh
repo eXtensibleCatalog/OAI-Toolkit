3b58b938b0d0bdb4ca6bb851b751df72b524c258
cedelis@uillinois.edu <cedelis@uillinois.edu@3eaa8e94-081b-11de-9c5c-7db7966a26d7>
2012-06-27 15:22:24 +0000

    The sample wasn't working properly.  Also, added a slightly-modified version of it (sans -production).
    git-svn-id: https://xcoaitoolkit.googlecode.com/svn/trunk@517 3eaa8e94-081b-11de-9c5c-7db7966a26d7

--- sample_scripts/convertmodifyload.sh
+++ sample_scripts/convertmodifyload.sh
@@ -7,4 +7,4 @@
 # OAIToolkit Manual for the details.
 # ==============================================
 
-java -Xmx1024m -cp "lib/xercesImpl.jar:lib/xml-apis.jar" -jar lib/OAIToolkit-1.0.8.jar -convert -modify drop_pipeline.xsl -load -production -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -lucene_index lucene_index
+java -Xmx1024m -cp "lib/xercesImpl.jar:lib/xml-apis.jar" -jar lib/OAIToolkit-1.0.8.jar -convert -modify drop_pipeline.xsl -load -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -lucene_index lucene_index
