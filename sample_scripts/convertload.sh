d766d0b0f287777c890d702c076f199bf8120e3e
cedelis@uillinois.edu <cedelis@uillinois.edu@3eaa8e94-081b-11de-9c5c-7db7966a26d7>
2012-12-05 18:13:46 +0000

    Forgot to include the script changes in my previous commit. Set up for 1.0.11 release.
    git-svn-id: https://xcoaitoolkit.googlecode.com/svn/trunk@529 3eaa8e94-081b-11de-9c5c-7db7966a26d7

--- sample_scripts/convertload.sh
+++ sample_scripts/convertload.sh
@@ -7,4 +7,4 @@
 # OAIToolkit Manual for the details.
 # ==============================================
 
-java -Xmx2g -cp "lib/xercesImpl.jar:lib/xml-apis.jar" -jar lib/OAIToolkit-1.0.10.jar -load -convert -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -marc_encoding utf8 -split_size 10000 -lucene_index lucene_index -translate_leader_bad_chars_to_zero -translate_nonleader_bad_chars_to_spaces -indent_xml
+java -Xmx2g -cp "lib/xercesImpl.jar:lib/xml-apis.jar" -jar lib/OAIToolkit-1.0.11.jar -load -convert -source marc -destination marc_dest -destination_xml xml -error error -error_xml error_xml -log log -log_detail -marc_schema schema/MARC21slim_rochester.xsd -marc_encoding utf8 -split_size 10000 -lucene_index lucene_index -translate_leader_bad_chars_to_zero -translate_nonleader_bad_chars_to_spaces -indent_xml
--- sample_scripts/convertmodifyloadproduction.bat
+++ sample_scripts/convertmodifyloadproduction.bat
@@ -7,4 +7,4 @@ rem parameters according to your needs. Read the
 rem OAIToolkit Manual for the details.
 rem ==============================================
 
-java -Xmx2g -cp "lib\xercesImpl.jar:lib\xml-apis.jar" -jar lib\OAIToolkit-1.0.10.jar -convert -modify drop_pipeline.xsl -load -production -log log -log_detail -marc_schema schema\MARC21slim_rochester.xsd -lucene_index lucene_index
+java -Xmx2g -cp "lib\xercesImpl.jar:lib\xml-apis.jar" -jar lib\OAIToolkit-1.0.11.jar -convert -modify drop_pipeline.xsl -load -production -log log -log_detail -marc_schema schema\MARC21slim_rochester.xsd -lucene_index lucene_index
